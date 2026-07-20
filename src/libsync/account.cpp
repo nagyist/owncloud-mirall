69c84a61354a2575fef10d28ed7ae1551e1ba7b8
modSpike <lisa.reese@kiteworks.com>
2026-07-20 16:12:22 +0200

    refactor: [DC-304] create an avatar if the system does not provide one (#12585)
    * decent start
    
    * a few improvements
    
    mostly just skipping any attempt to pick a color, just use the say gray tones we use on other icons for the avatar
    
    * fixed a bug in re-auth
    
    related to demo only. For unknown reasons on reauth we get an empty auth url back from demo. this used to assert but since I finally hit it, I updated the handling to add an error message and let the user "stay logged out"
    
    * update some comments

--- src/libsync/account.cpp
+++ src/libsync/account.cpp
@@ -128,20 +128,24 @@ QString Account::davUser() const
     return _davUser;
 }
 
-QIcon Account::avatar() const
+QIcon Account::avatar()
 {
     if (_avatarImg.isNull()) {
-        // for now
-        return Resources::getCoreIcon("check");
-        // near future with DC-304:
-        // setAvatar(Resources::buildAvatar(this);
-        // or so
+        // note this caches the built avatar, so once it exists it just returns a cached version.
+        return Resources::buildAvatar(initials(), uuid());
     }
     return _avatarImg;
 }
 
 void Account::setAvatar(const QIcon &img)
 {
+    // this excludes the condition that we get a null image on every. single. run. of the fetchServerSettings, which
+    // is also responsible for fetching the avatar iff the caps show avatars are supported.
+    // however we are going to have the same problem with non-null avatars, and comparing them to see if the new one really
+    // is different is much more complicated and needs a deeper fix.
+    // see dc-324 for final solution
+    if (img.isNull() && _avatarImg.isNull())
+        return;
     _avatarImg = img;
     Q_EMIT avatarChanged();
 }
